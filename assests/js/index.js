c0dbd671253c8e099e838c4478decee3dfabd163
ayushkrtech <techarunprd@gmail.com>
2024-05-30 22:18:57 +0530

    Update index.js

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,1 +1,9 @@
+  function myMenuFunction(){
+    var menubtn=document.getElementById("myNavMenu");
 
+    if(menubtn.className === "nav-menu"){
+        menubtn.className += "responsive";
+    } else{
+        menubtn.className = "nav-menu";
+    }
+  }
