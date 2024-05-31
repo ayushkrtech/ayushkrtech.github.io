aefd8764a18061121f4b856274b4d47f14763cbd
Ayush <techarunprd@gmail.com>
2024-05-31 09:49:59 +0530

    Update index.js

--- assests/js/index.js
+++ assests/js/index.js
@@ -7,3 +7,19 @@
         menubtn.className = "nav-menu";
     }
   }
+/* ---- shadow effect --- */
+    window.onscroll = function() {headerShadow()};
+
+    function headerShadow( ) {
+      const navheader = document.getElementById("header");
+
+      if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navheader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)"
+        navheader.style.height = "70px"
+        navheader.style.lineHeight = "70px"
+      } else {
+        navheader.style.boxShadow = "none"
+        navheader.style.height = "90px"
+        navheader.style.lineHeight = "90px"
+      }
+    }
